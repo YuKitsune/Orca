bc42c80ee5d88c428327261c4ffc977ddbdb46f7
Eoin Motherway <eoin.motherway@octopus.com>
2020-12-23 13:51:34 +1000

    Moved CheckCommits and CheckTextBody

--- pkg/scanning/scanner.go
+++ pkg/scanning/scanner.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/base64"
 	"github.com/google/go-github/v33/github"
+	"log"
 	"strings"
 )
 
@@ -57,6 +58,72 @@ func NewScanner(patternStore *PatternStore) (*Scanner, error) {
 	}
 
 	return scanner, nil
+}
+
+
+func (scanner *Scanner) CheckCommits(
+	repo *github.Repository,
+	githubClient *github.Client,
+	commits []*github.RepositoryCommit) ([]CommitScanResult, error) {
+
+	var commitScanResults []CommitScanResult
+	for _, commit := range commits {
+
+		// NOTE: ListCommits does not include any references to which files were changed (commit.Files is always nil),
+		//	so we need to send another request specifically for the commit
+		// TODO: Find a way around this to prevent getting rate limited
+		commitScanResult := CommitScanResult{Commit: *commit.SHA}
+		commitWithFiles, _, err := githubClient.Repositories.GetCommit(
+			context.Background(),
+			*repo.Owner.Login,
+			*repo.Name,
+			*commit.SHA)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, file := range commitWithFiles.Files {
+
+			// Only care about added and modified files
+			if *file.Status != "added" && *file.Status != "modified" {
+				continue
+			}
+
+			log.Printf("Checking %s from %s", *file.Filename, *commit.SHA)
+
+			fileContentMatch, err := scanner.CheckFileContentFromCommit(
+				githubClient,
+				repo.Owner.Login,
+				repo.Name,
+				commit.SHA,
+				file.Filename)
+			if err != nil {
+				return nil, err
+			}
+
+			if len(fileContentMatch.LineMatches) > 0 {
+				commitScanResult.Matches = append(commitScanResult.Matches, *fileContentMatch)
+			}
+		}
+
+		if commitScanResult.HasMatches() {
+			commitScanResults = append(commitScanResults, commitScanResult)
+		}
+	}
+	return commitScanResults, nil
+}
+
+func (scanner *Scanner) CheckTextBody(body *string) (*ContentMatch, error) {
+	var result ContentMatch
+	contentResult, err := scanner.checkContent(body)
+	if err != nil {
+		return nil, err
+	}
+	if len(contentResult.LineMatches) > 0 {
+		result.LineMatches = contentResult.LineMatches
+	}
+
+	return &result, nil
 }
 
 func (scanner *Scanner) CheckFileContentFromCommit(
